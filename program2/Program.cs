23bafc7b4cb20448da97666f9c1952fe5ab7b8cc
Kostya <relond4186@gmail.com>
2022-08-08 14:19:51 +0300

    Первая программа

--- program2/Program.cs
+++ program2/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine(", HelloWorld!");
+﻿//Console.Clear();
+//Console.Write ("Введите  число ");
+//int a = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Введите 2 число ");
+//int b = Convert.ToInt32(Console.ReadLine());
+//int с = Convert.ToInt32(Math.Max(a , b));
+//Console.WriteLine("Максимальное число  "+ с);

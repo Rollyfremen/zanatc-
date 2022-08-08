859b90585f0c07cb073e25d4c2a9a694d63d09b3
Kostya <relond4186@gmail.com>
2022-08-08 15:07:54 +0300

    все домашние задачи

--- program2/Program.cs
+++ program2/Program.cs
@@ -1,10 +1,77 @@
-﻿Console.Clear();
-Console.Write ("Введите  число ");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите 2 число ");
-int b = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите 3 число ");
-int d = Convert.ToInt32(Console.ReadLine());
-int с = Convert.ToInt32(Math.Max(a , b));
-int g = Convert.ToInt32(Math.Max(Math.Max(a , b), d));
-Console.WriteLine("Максимальное число  "+ g);
+﻿
+
+//Первая задача на вывод большего и меньшего 
+
+//Console.Clear();
+//Console.Write("Введите 1 число ");
+//int a = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Введите 2 число ");
+//int b = Convert.ToInt32(Console.ReadLine());
+//if (a>b) 
+//{ 
+//Console.WriteLine("Большее число "+ a);
+ //   Console.WriteLine("Меньшее число "+ b);
+//}
+ //  else 
+  // {
+  //  Console.WriteLine("Большее число "+ b);
+   // Console.WriteLine("Меньшее число "+ a);
+//}
+
+
+
+
+
+
+
+
+
+
+// Вторая задача на нахождение максимального числа из 3 введеных 
+
+//Console.Clear();
+//Console.Write ("Введите  число ");
+//int a = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Введите 2 число ");
+//int b = Convert.ToInt32(Console.ReadLine());
+//.Write("Введите 3 число ");
+// d = Convert.ToInt32(Console.ReadLine());
+//int с = Convert.ToInt32(Math.Max(a , b));
+//int g = Convert.ToInt32(Math.Max(Math.Max(a , b), d));
+//Console.WriteLine("Максимальное число  "+ g);
+
+
+
+
+
+
+// Задание номер три показывает четное или не четное число 
+//Console.Clear();
+//Console.Write("Введите число : ");
+//int i= Convert.ToInt32(Console.ReadLine());
+//  if (i % 2 == 0)
+//{
+//Console.Write("Введенное число является четным");
+//Console.Read();
+//}
+//  else
+//{
+//    Console.Write("Введенное число нечетное");
+//   Console.Read();
+// }
+
+
+
+// Задача 4 на нахождение всех четных в диапозоне от n до m 
+//Console.Clear();
+//Console.Write("Введите число 1: "); 
+//int n = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Введите число 2: ");
+//int m = Convert.ToInt32(Console.ReadLine());
+//for (int i = n; i <= m; i++)
+//{
+//if (i % 2 == 0) 
+//{
+//Console.Write(i + " ");
+//}
+//}

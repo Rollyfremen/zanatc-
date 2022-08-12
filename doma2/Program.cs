8746de51567f2f0a5b10e6abebbf6fb54dba0812
Kostya <relond4186@gmail.com>
2022-08-12 22:22:40 +0300

    дЗ за второй семинар

---
+++ doma2/Program.cs
@@ -0,0 +1,69 @@
+﻿
+// Первая задача на вывод второго числа 
+ 
+
+// Console.WriteLine("Введите число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// if(a >= 10)
+// {
+//     int b = a / 10 % 10;
+//     Console.WriteLine("Второе число "+ b);
+// }
+// else 
+// {
+//   Console.WriteLine("Второго числа нет");
+// }
+
+
+
+//Вторая задача на нахождение третьего числа 
+
+// Console.WriteLine("Введите число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// if(a<100)
+// {
+//     Console.WriteLine("Третьей цифры нет");
+// }
+// else
+// {
+//      int b = a % 10;
+//      Console.WriteLine("Треться цифра "+ b);
+// }
+
+
+// Третья задача на проверку но номеру является ли день выходным 
+
+// Console.WriteLine("Введите номер дня от 1 до 7");
+// int a = Convert.ToInt32(Console.ReadLine());
+// if (a ==1 )
+// {
+//     Console.WriteLine("Понеделник");
+// }
+// else if (a ==2 )
+// {
+//     Console.WriteLine("Вторник");
+// }
+// else if (a ==3 )
+// {
+//     Console.WriteLine("Среда");
+// }
+// else if (a ==4 )
+// {
+//     Console.WriteLine("Четверг");
+// }
+// else if (a ==5 )
+// {
+//     Console.WriteLine("Пятница");
+// }
+// else if (a ==6 )
+// {
+//     Console.WriteLine("Суббота выходной");
+// }
+// else if (a ==7 )
+// {
+//     Console.WriteLine("Воскресенье выходной");
+// }
+// else 
+// {
+//         Console.WriteLine("Вы введи неверное число");
+// }

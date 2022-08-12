a85fe213bed3d2cefab1c568fd6c02b87070b549
Kostya <relond4186@gmail.com>
2022-08-12 23:15:29 +0300

    Третья домашка

---
+++ doma3/Program.cs
@@ -0,0 +1,55 @@
+﻿
+// первое задание на нахождение палиндрома 
+
+// Console.WriteLine("Введите пятизначное число");
+// int a = Convert.ToInt32(Console.ReadLine());
+//  int b = a/10000;
+// int c = a%10;
+// int d = a/1000%10;
+// int s = a/10%10;
+// if (a>=10000)
+// {
+//      if ( b == c && d == s )
+//     {
+//          Console.WriteLine("Число является палиндромом");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Число не является палиндромом");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("Вы ввели не верное число");
+// }
+
+
+// Вторая задача на нахождение растояние в 3d пространстве 
+
+
+// Console.WriteLine("Введите координату x для первой точки");
+// int Xa = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату y для первой точки");
+// int Ya = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату z для первой точки");
+// int Za = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату x для второй точки");
+// int Xb = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату y для второй точки");
+// int Yb = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координату z для второй точки");
+// int Zb = Convert.ToInt32(Console.ReadLine());
+// double ras = Math.Sqrt(Math.Pow((Xb-Xa), 2)+ Math.Pow((Yb-Ya), 2)+Math.Pow((Zb-Za),2));
+// Console.WriteLine("Расстояние между точками равно "+ ras);
+
+
+//Третья задача на нахождение куба от 1 до n
+
+// Console.WriteLine("Введите число");
+// int kvar = Convert.ToInt32(Console.ReadLine());
+// int i = 1;
+// while (i <= kvar)
+// {
+//     Console.WriteLine(Math.Pow(i , 3));
+//     i++; 
+// }

3bbd35ab8b9288ad59f76716e12d534f06943725
Kostya <relond4186@gmail.com>
2022-08-27 13:35:31 +0300

    Вторая задача

--- doma4/Program.cs
+++ doma4/Program.cs
@@ -1,6 +1,21 @@
-﻿Console.WriteLine("Введите число которое нужно возвести в степень");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите исло в степень которого нужно возвести");
-int a = Convert.ToInt32(Console.ReadLine());
-double d = Math.Pow(m, a);
-Console.WriteLine(d);
+﻿// Console.WriteLine("Введите число которое нужно возвести в степень");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите исло в степень которого нужно возвести");
+// int a = Convert.ToInt32(Console.ReadLine());
+// double d = Math.Pow(m, a);
+// Console.WriteLine(d);
+
+
+
+
+
+
+// Console.WriteLine("Введите число");
+// int m = Convert.ToInt32(Console.ReadLine());
+// int s = 0;
+// while(m>0)
+// {
+//     s = s + m %10;
+//     m /=10;
+// }
+// Console.WriteLine("Cумма цифр равна "+ s);

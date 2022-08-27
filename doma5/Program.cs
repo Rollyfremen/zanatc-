9cd88f712dd362959ee117548a6ecb5edb3febcd
Kostya <relond4186@gmail.com>
2022-08-27 15:17:13 +0300

    Первая задача

---
+++ doma5/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] array = new int[10];
+int count= 0;
+for(int i = 0 ; i <  array.Length; i++)
+{
+  array[i]= new Random().Next(100,1000);
+  Console.WriteLine(array[i]);
+  if(array[i] % 2 == 0)
+  {
+      count = count+ 1;
+  }
+}
+Console.WriteLine("Колличество четных чисел "+count);

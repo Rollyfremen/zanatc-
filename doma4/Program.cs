9ba0ebd86e5b3b19d2eaf719feb616c5dbd0cb43
Kostya <relond4186@gmail.com>
2022-08-27 13:20:54 +0300

    первая задача

---
+++ doma4/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число которое нужно возвести в степень");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите исло в степень которого нужно возвести");
+int a = Convert.ToInt32(Console.ReadLine());
+double d = Math.Pow(m, a);
+Console.WriteLine(d);
